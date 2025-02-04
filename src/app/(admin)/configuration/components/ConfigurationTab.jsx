583beb3f517ba31a43a0400c3dab03d8b11fb5ec
Manish-fundtec <manish.sahu@fundtec.in>
2025-02-04 19:35:01 +0530

    Revert "update"
    This reverts commit d1616cc0f8257b5b94df871db54010118a9d1a2c.

--- src/app/(admin)/configuration/components/ConfigurationTab.jsx
+++ src/app/(admin)/configuration/components/ConfigurationTab.jsx
@@ -1,44 +1,33 @@
 'use client';
 
 import IconifyIcon from '@/components/wrappers/IconifyIcon';
-import { useState, useEffect } from 'react';
-import { Col, Nav, NavItem, NavLink, Row, Tab, TabContainer, TabContent, TabPane } from 'react-bootstrap';
+import { Col, Nav, NavItem, NavLink, Row, Tab, TabContainer, TabContent, TabPane, Tabs } from 'react-bootstrap';
 import { tabContents } from './data';
-
 export const NavPills = () => {
-  // Retrieve the last selected tab from localStorage or default to the first tab
-  const [activeTab, setActiveTab] = useState<string>(() => {
-    return localStorage.getItem('activeTab') || '1';
-  });
+    return (
+        <TabContainer defaultActiveKey={'1'}>
+          <Nav as={'ul'} variant="pills">
+            {tabContents.map((tab, idx) => <NavItem as={'li'} key={idx}>
+                <NavLink eventKey={tab.id}>
+                  <span className="d-block d-sm-none">
+                    <IconifyIcon icon={tab.icon} />
+                  </span>
+                  <span className="d-none d-sm-block">{tab.title}</span>
+                </NavLink>
+              </NavItem>)} 
+          </Nav>
+          <TabContent className="pt-2 text-muted">
+            {tabContents.map((tab, idx) => <TabPane eventKey={tab.id} key={idx}>
+                <p className="mb-0">{tab.description}</p>
+              </TabPane>)}
+          </TabContent>
+        </TabContainer>
+      );
+  };
 
-  // Update localStorage when tab changes
-  useEffect(() => {
-    localStorage.setItem('activeTab', activeTab);
-  }, [activeTab]);
-
-  return (
-    <TabContainer activeKey={activeTab} onSelect={(key) => setActiveTab(key || '1')}>
-      <Nav as="ul" variant="pills">
-        {tabContents.map((tab, idx) => (
-          <NavItem as="li" key={idx}>
-            <NavLink eventKey={tab.id}>
-              <span className="d-block d-sm-none">
-                <IconifyIcon icon={tab.icon} />
-              </span>
-              <span className="d-none d-sm-block">{tab.title}</span>
-            </NavLink>
-          </NavItem>
-        ))}
-      </Nav>
-      <TabContent className="pt-2 text-muted">
-        {tabContents.map((tab, idx) => (
-          <TabPane eventKey={tab.id} key={idx}>
-            <p className="mb-0">{tab.description}</p>
-          </TabPane>
-        ))}
-      </TabContent>
-    </TabContainer>
-  );
-};
-
-export default NavPills;
+  const configurationTabs = () => {
+    return <>
+        <NavPills />
+      </>;
+  };
+  export default configurationTabs;
