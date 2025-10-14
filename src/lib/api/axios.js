34cbbf0f9a23a363e3385fc500c16411be2684bb
Manish-fundtec <manish.sahu@fundtec.in>
2025-10-14 13:39:57 +0530

    Update axios.js

--- src/lib/api/axios.js
+++ src/lib/api/axios.js
@@ -1,8 +1,18 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('userToken') || Cookies.get('dashboardToken');
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 
 export default api;
